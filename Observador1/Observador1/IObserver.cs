4529dfb3d0ea3fcd242b18734df00a52b9f9b126
Craituz <68795431+Craituz@users.noreply.github.com>
2020-08-07 15:30:49 -0500

    Add files via upload

---
+++ Observador1/Observador1/IObserver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observador1
+{
+    interface IObserver
+    {
+        void Update(string mensaje);
+        //este metodo upsate se usa en el modelo de Push
+        //void Updatepull();
+        //en otro caso podriamos usar Updatepull del modelo de pull
+    }
+}
